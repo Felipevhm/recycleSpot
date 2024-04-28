174ef210ddd24e7013c5431798cb75eaad4b6e0b
Felipe <fvhmanhaes@gmail.com>
2024-04-27 22:36:47 -0300

    Finish the CRUD with save button knowing whther user exists or not by the nameExists function

--- src/context/UsersContext.jsx
+++ src/context/UsersContext.jsx
@@ -1,82 +1,122 @@
-import {createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const UsersContext = createContext()
+export const UsersContext = createContext();
 
-export const UsersContextProvider = ({children})=>{
-   const [users, setUsers] = useState([]);
+export const UsersContextProvider = ({ children }) => {
+  const blankUser = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState(blankUser);
 
-   useEffect(() => {
-      getusers()
-   }, []);
+  useEffect(() => {
+    getusers();
+  }, []);
 
-   function getusers() {
-       fetch("http://localhost:3000/users")
-       .then((response) => response.json())
-       .then((data) => setUsers(data))
-       .catch((error) => console.log(error));    
+  function nameExists(arrayOfObjects, name) {
+   const foundUser = arrayOfObjects.find((user) => user.name === name);
+   if (foundUser) {
+     return {result:true, foundUser:foundUser};
+   } else {
+     return {result:false, foundUser:null};
    }
-  
-   function getuserById(id) {
-      fetch("http://localhost:3000/users/"+id)
+ }
+ 
+
+  function getusers() {
+    fetch("http://localhost:3000/users")
       .then((response) => response.json())
       .then((data) => setUsers(data))
-      .catch((error) => console.log(error));    
+      .catch((error) => console.log(error));
   }
 
-   function registerUser(user){
-      if (user.name == ""){
-         alert('User field needs a name!')
-      }
-      setUsers ([...users,user])
-      fetch("http://localhost:3000/users",{
-         method: 'POST',
-         body: JSON.stringify(user),
-         headers: {'Content-Type': 'application/json'}
-      })
-      .then(() => {
-         window.alert('User registered successfully!')
-         getusers()
-         })
-      .catch((error) => window.alert('User not registered!',error))
-      
-     
-   }
+  function getuserById(id) {
+    console.log("id IS: ");
+    console.log(id);
 
-   function deleteUser(id){
-      fetch("http://localhost:3000/users/"+id,{
-         method: 'DELETE',
+    fetch("http://localhost:3000/users/" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers([data]);
+        setNewUser(data);
       })
-      .then(() => {
-         window.alert('User deleted successfully!')
-         getusers()
-         })
-      .catch((error) => window.alert('User not deleted!',error))
-   }
+      .catch((error) => console.log(error));
+  }
 
-   function updateUser(user,id){
-      if (user.name == ""){
-         alert('User field needs a name!')
-      }
-      fetch("http://localhost:3000/users/"+id,{
-         method: 'PUT',
-         body: JSON.stringify(user),
-         headers: {'Content-Type': 'application/json'}
+  function registerUser(user) {
+   const searchResult = nameExists(users, user.name)
+
+    if (user.name == "") {
+      alert("User field needs a name!");
+    }
+    if (searchResult.result) {
+      alert("Name already in use and will be updated!");
+      updateUser(user, searchResult.foundUser.id);
+    } else {
+      alert("Name not yet used!");
+ 
+      setUsers([...users, user]);
+      fetch("http://localhost:3000/users", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-Type": "application/json" },
       })
+        .then(() => {
+          window.alert("User registered successfully!");
+          getusers();
+        })
+        .catch((error) => window.alert("User not registered!", error));
+    }
+  }
+
+  function deleteUser(id) {
+    fetch("http://localhost:3000/users/" + id, {
+      method: "DELETE",
+    })
       .then(() => {
-         window.alert('User updated successfully!')
-         getusers()
-         })
-      .catch((error) => window.alert('User not registered!',error))
-      
-     
-   }
-   
-   return(
-      <>
-      <UsersContext.Provider value={{
-         users,registerUser,deleteUser,updateUser,getuserById}}>
-         {children}
+        window.alert("User deleted successfully!");
+        setNewUser(blankUser);
+        getusers();
+      })
+      .catch((error) => window.alert("User not deleted!", error));
+  }
+
+  function updateUser(user, id) {
+    if (user.name == "") {
+      alert("User field needs a name!");
+    }
+    fetch("http://localhost:3000/users/" + id, {
+      method: "PUT",
+      body: JSON.stringify(user),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(() => {
+        window.alert("User updated successfully!");
+        getusers();
+      })
+      .catch((error) => window.alert("User not registered!", error));
+  }
+
+  return (
+    <>
+      <UsersContext.Provider
+        value={{
+          users,
+          newUser,
+          blankUser,
+          nameExists,
+          setNewUser,
+          registerUser,
+          deleteUser,
+          updateUser,
+          getusers,
+          getuserById,
+        }}
+      >
+        {children}
       </UsersContext.Provider>
-      </>
-   )
-}
+    </>
+  );
+};
