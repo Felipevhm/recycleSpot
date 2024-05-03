bf9333ffbaa3c425686e6604213910b00839db0f
Felipe <fvhmanhaes@gmail.com>
2024-05-02 23:05:04 -0300

    Include Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import RegisterUser from "./pages/RegisterUser";
 import { Outlet } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   
   return (
     <>
-    <Outlet/>
+    <Header />
+    <Outlet />
+    <Footer />
         </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+import { Link } from "react-router-dom";
+
+
+function Footer() {
+  return (
+    <div className="footer-class">
+      <Link to="/">Recycle Spot</Link>
+      <div className="links">
+      <Link to="/register-collect-point">Register Collect Point</Link>
+
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
